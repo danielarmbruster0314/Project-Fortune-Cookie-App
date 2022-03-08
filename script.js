f01139a6ee3b50c15f1ebde40f583977ed27fcc0
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-08 11:39:50 -0700

    setting up button event listners

--- script.js
+++ script.js
@@ -1,55 +1,59 @@
-addEventListener("load",app);
-function app() {
-	class Fortune {
-		constructor(fortuneList) {
-			this.text = !fortuneList ? "No fortune" : fortuneList[~~(Math.random() * fortuneList.length)];
-			this.luckyNumbers = [];
-			this.drawLuckyNumbers();
-		}
-		drawLuckyNumbers() {
-			let maxDraws = 6,
-				draws = maxDraws,
-				maxNumber = 99,
-				numberPool = [];
+addEventListener("load",init);
+function init() {
+	// class Fortune {
+	// 	constructor(fortuneList) {
+	// 		this.text = !fortuneList ? "No fortune" : fortuneList[~~(Math.random() * fortuneList.length)];
+	// 		this.luckyNumbers = [];
+	// 		this.drawLuckyNumbers();
+	// 	}
+	// 	drawLuckyNumbers() {
+	// 		let maxDraws = 6,
+	// 			draws = maxDraws,
+	// 			maxNumber = 99,
+	// 			numberPool = [];
 
-			// create number pool
-			while (maxNumber--) {
-				numberPool.unshift(maxNumber + 1);
-			}
-			// draw from pool, populate the lucky numbers
-			while (draws--) {
-				let drawn = ~~(Math.random() * numberPool.length);
-				this.luckyNumbers.push(numberPool[drawn]);
-				numberPool.splice(drawn,1);
-			}
-		}
-	}
+	// 		// create number pool
+	// 		while (maxNumber--) {
+	// 			numberPool.unshift(maxNumber + 1);
+	// 		}
+	// 		// draw from pool, populate the lucky numbers
+	// 		while (draws--) {
+	// 			let drawn = ~~(Math.random() * numberPool.length);
+	// 			this.luckyNumbers.push(numberPool[drawn]);
+	// 			numberPool.splice(drawn,1);
+	// 		}
+	// 	}
+	// }
+    const regularbttn = document.querySelector('#fbttn');
+    const wisdombttn = document.querySelector('#wbttn');
+    const vocabbttn = document.querySelector('#vbttn');
+    const lotterybttn = document.querySelector('#lbttn');
 
-	var fcBtn = document.querySelector("button"),
+	let fcBtn = document.querySelector("button"),
 		fortuneText = document.querySelector(".fc-fortune-text"),
 		fortuneLuckyNumbers = document.querySelector(".fc-lucky-numbers span"),
-		fortuneList = [
-			"Did you know that fortune cookies aren’t even Chinese? They’re made by Americans, based on a Japanese recipe.",
-			"You have an ability to sense and know higher truth.",
-			"Excellent time to become a missing person.",
-			"Don’t look back, the lemmings are gaining on you.",
-			"You look tired.",
-			"Chess tonight.",
-			"Don’t feed the bats tonight.",
-			"Your boss is a few sandwiches short of a picnic.",
-			"You will forget that you ever knew me.",
-			"Future looks spotty. You will spill soup in late evening.",
-			"You have the capacity to learn from mistakes. You’ll learn a lot today.",
-			"A vivid and creative mind characterizes you.",
-			"If you think last Tuesday was a drag, wait till you see what happens tomorrow!",
-			"You will be attacked next Wednesday at 3:15 P.M. by six samurai sword wielding purple fish glued to Harley-Davidson motorcycles.",
-			"There was a phone call for you.",
-			"Are you making all this up as you go along?",
-			"Don’t plan any hasty moves. You’ll be evicted soon anyway.",
-			"Advancement in position.",
-			"You’re ugly and your mother dresses you funny.",
-			"Be careful! Is it classified?"
-		],
+		// fortuneList = [
+		// 	"Did you know that fortune cookies aren’t even Chinese? They’re made by Americans, based on a Japanese recipe.",
+		// 	"You have an ability to sense and know higher truth.",
+		// 	"Excellent time to become a missing person.",
+		// 	"Don’t look back, the lemmings are gaining on you.",
+		// 	"You look tired.",
+		// 	"Chess tonight.",
+		// 	"Don’t feed the bats tonight.",
+		// 	"Your boss is a few sandwiches short of a picnic.",
+		// 	"You will forget that you ever knew me.",
+		// 	"Future looks spotty. You will spill soup in late evening.",
+		// 	"You have the capacity to learn from mistakes. You’ll learn a lot today.",
+		// 	"A vivid and creative mind characterizes you.",
+		// 	"If you think last Tuesday was a drag, wait till you see what happens tomorrow!",
+		// 	"You will be attacked next Wednesday at 3:15 P.M. by six samurai sword wielding purple fish glued to Harley-Davidson motorcycles.",
+		// 	"There was a phone call for you.",
+		// 	"Are you making all this up as you go along?",
+		// 	"Don’t plan any hasty moves. You’ll be evicted soon anyway.",
+		// 	"Advancement in position.",
+		// 	"You’re ugly and your mother dresses you funny.",
+		// 	"Be careful! Is it classified?"
+		// ],
 		fortune = new Fortune(),
 
 		getFortune = function(){
@@ -77,4 +81,9 @@ function app() {
 	
 	getFortune();
 	fcBtn.addEventListener("click",nextState);
+
+
+
+
+
 }
